fc524f4a0c6e7b3e4f43213f01a834efc5de70d8
obostjancic <ognjen.bostjancic@gmail.com>
2021-12-14 21:20:55 +0100

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -7,22 +7,20 @@ import { ProjectRepository } from "./repositories/project.repository";
 import { GroupService } from "./services/group.service";
 import { ProjectService } from "./services/project.service";
 import { WorkspaceConfigService } from "./services/workspaceConfig.service";
-import { dashboardContent } from "./ui/dashboard";
-
+import {
+  CREATE_GROUP,
+  CREATE_PROJECT,
+  DASHBOARD_VIEW_ID,
+  DELETE_GROUP,
+  DELETE_PROJECT,
+  OPEN_DASHBOARD,
+  UPDATE_GROUP,
+  UPDATE_PROJECT,
+} from "./ui/consts";
+// import { dashboardContent } from "./ui/dashboard";
+import { DashboardPanel } from "./ui/DashboardPanel";
 // TODO: Workspace <-> Project relation needs rethinking, update function cleanup
 // TODO: Configuration access layer is needed
-
-const CREATE_GROUP = "dash.createGroup";
-const UPDATE_GROUP = "dash.updateGroup";
-const DELETE_GROUP = "dash.deleteGroup";
-
-const OPEN_PROJECT = "dash.openProject";
-const CREATE_PROJECT = "dash.createProject";
-const UPDATE_PROJECT = "dash.updateProject";
-const DELETE_PROJECT = "dash.deleteProject";
-
-const OPEN_DASHBOARD = "dash.openDashboard";
-const DASHBOARD_VIEW_ID = "dash.dashboard";
 
 export const init = async (context: vscode.ExtensionContext) => {
   const getCurrentPath = () => vscode.workspace.workspaceFolders?.[0].uri.path ?? "";
@@ -62,69 +60,16 @@ class SidebarDummyDashboardViewProvider implements vscode.WebviewViewProvider {
   };
 }
 
-class DashboardSerializer implements vscode.WebviewPanelSerializer {
-  constructor(private readonly context: vscode.ExtensionContext) {}
-
-  async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
-    console.log(`Got state: ${state}`);
-    const { groupService, projectService } = state;
-
-    webviewPanel.webview.html = await dashboardContent(
-      groupService,
-      projectService,
-      this.context,
-      webviewPanel.webview
-    ); //dashboardContent();
-  }
-}
-
 export async function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "dash" is now active!');
 
   const { groupService, projectService, commands } = await init(context);
-  const serializer = new DashboardSerializer(context);
-  let panel: vscode.WebviewPanel;
-
   const openDashboard = () => {
-    panel = vscode.window.createWebviewPanel(DASHBOARD_VIEW_ID, "Dashboard", vscode.ViewColumn.One, {
-      enableScripts: true,
-    });
-    eventHandler(panel.webview);
-    renderDashboard();
+    DashboardPanel.render(context.extensionUri, { groupService, projectService, commands });
   };
-
-  const renderDashboard = async () => {
-    serializer.deserializeWebviewPanel(panel, { groupService, projectService });
-  };
-
-  const eventHandler = (webview: vscode.Webview) =>
-    webview.onDidReceiveMessage(async (e: any) => {
-      switch (e.type) {
-        case OPEN_PROJECT:
-          await commands.openProject(e.payload);
-          break;
-        case DELETE_GROUP:
-          await commands.deleteGroup(e.payload);
-          break;
-        case CREATE_PROJECT:
-          await commands.createProject(e.payload);
-          break;
-        case DELETE_PROJECT:
-          await commands.deleteProject(e.payload);
-          break;
-        case UPDATE_PROJECT:
-          await commands.updateProject(e.payload);
-          break;
-        case UPDATE_GROUP:
-          await commands.updateGroup(e.payload);
-          break;
-      }
-      renderDashboard();
-    });
 
   const provider = new SidebarDummyDashboardViewProvider(context.extensionUri);
   context.subscriptions.push(vscode.window.registerWebviewViewProvider(DASHBOARD_VIEW_ID, provider));
-  vscode.window.registerWebviewPanelSerializer(DASHBOARD_VIEW_ID, serializer);
 
   registerCommands();
   function registerCommands() {
---
+++ src/ui/consts.ts
@@ -0,0 +1,11 @@
+export const CREATE_GROUP = "dash.createGroup";
+export const UPDATE_GROUP = "dash.updateGroup";
+export const DELETE_GROUP = "dash.deleteGroup";
+
+export const OPEN_PROJECT = "dash.openProject";
+export const CREATE_PROJECT = "dash.createProject";
+export const UPDATE_PROJECT = "dash.updateProject";
+export const DELETE_PROJECT = "dash.deleteProject";
+
+export const OPEN_DASHBOARD = "dash.openDashboard";
+export const DASHBOARD_VIEW_ID = "dash.dashboard";
