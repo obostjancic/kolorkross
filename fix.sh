b55d33644637d9c73689a4692daf60a5eeec3b18
obostjancic <ognjen.bostjancic@gmail.com>
2021-12-18 13:06:13 +0100

    WIP

--- fix.sh
+++ fix.sh
@@ -1,11 +1,25 @@
-git filter-branch --commit-filter "
-        if [ "$GIT_COMMITTER_NAME" = "ognjen-anyline" ];
-        then
-                GIT_COMMITTER_NAME="obostjancic";
-                GIT_AUTHOR_NAME="obostjancic";
-                GIT_COMMITTER_EMAIL="ognjen.bostjancic@gmail.com";
-                GIT_AUTHOR_EMAIL="ognjen.bostjancic@gmail.com";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi" HEAD
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+an="$GIT_AUTHOR_NAME"
+am="$GIT_AUTHOR_EMAIL"
+cn="$GIT_COMMITTER_NAME"
+cm="$GIT_COMMITTER_EMAIL"
+
+if [ "$GIT_COMMITTER_EMAIL" = "ognjen@anyline.com" ]
+then
+    cn="obostjancic"
+    cm="ognjen.bostjancic@gmail.com"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "ognjen@anyline.com" ]
+then
+    an="obostjancic"
+    am="ognjen.bostjancic@gmail.com"
+fi
+
+export GIT_AUTHOR_NAME="$an"
+export GIT_AUTHOR_EMAIL="$am"
+export GIT_COMMITTER_NAME="$cn"
+export GIT_COMMITTER_EMAIL="$cm"
+'
