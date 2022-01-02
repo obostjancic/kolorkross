bf0762fe07ed6b5378796ac9846eb959c926c2a7
obostjancic <ognjen.bostjancic@gmail.com>
2022-01-02 16:35:59 +0100

    Removed ts expect error

--- src/util/matchers.ts
+++ src/util/matchers.ts
@@ -1,6 +1,5 @@
 export const partialMatch = <T>(p: T, q: Partial<T>): boolean => {
-  for (const key of Object.keys(q)) {
-    //@ts-expect-error - find a way to make this work
+  for (const key of Object.keys(q) as Array<keyof T>) {
     if (p[key] !== q[key]) {
       return false;
     }
